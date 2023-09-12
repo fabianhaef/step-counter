7ed6c49786e7a9885e8a96d58448505f46f56b4b
fabianhaef <fabian.haefi@gmail.com>
2023-09-12 23:51:20 +0200

    basic ring progress component

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Value from './src/components/Value';
+import RingProgress from './src/components/RingProgress';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <RingProgress radius={100} strokeWidth={35} progress={0.25}/>
       <View style={styles.values}>
         <Value label="Steps" value="1210" />
         <Value label="Distance" value="12" />
@@ -26,6 +28,7 @@ const styles = StyleSheet.create({
   values: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    marginTop: 100,
     gap: 50,
   },
 });
