e7c6fb60b8b9d8c43913401e5ffb31eb0ecf4d90
fabianhaef <fabian.haefi@gmail.com>
2023-09-12 23:00:42 +0200

    Render markdown and value component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type ValueProps = {
+  label: string;
+  value: string;
+}
+
+const Value = ({ label, value }: ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <Value label="Steps" value="12" />
+        <Value label="Distance" value="12" />
+      </View>
+
+      <Value label="Flights climbed" value="12" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#0c0c0c',
     justifyContent: 'center',
+    padding: 12,
   },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight: '500',
+  }
 });
